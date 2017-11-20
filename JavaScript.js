cf28fadb8fef46638c8f8c8b57da0c804673344e
AkshathaKShetty <31784177+AkshathaKShetty@users.noreply.github.com>
2017-11-20 07:06:40 +0530

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,20 @@
+﻿function Display() {
+    var idName = document.getElementsByTagName("form");
+    if (idName[0].id === "showHome") {
+        var formObj = document.getElementById("showHome");
+        if (formObj.style.display === "none" || formObj.style.display === "") {
+            formObj.style.display = "block";        
+        }
+    } else if (idName[0].id == "showForm")
+    {
+        var formObj = document.getElementById("showForm");
+        if (formObj.style.display === "none" || formObj.style.display === "") {
+            formObj.style.display = "block";
+        }
+    }
+    //var mobileName = document.getElementById("txtMobileName").
+}
+
+function redirect() {
+    window.location.hash = "#Form1.html";
+}
